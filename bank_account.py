122cb13d8d338773b4ded578216be8236fcdf9f4
rosebud-lang <rhosebhurdawuah@gmail.com>
2026-07-25 19:21:21 +0100

    creating a class for bank_account

---
+++ bank_account.py
@@ -0,0 +1,7 @@
+class BankAccount:
+    """Represents a customer's bank account."""
+
+    def __init__(self, name, balance):
+        """Initializes the account holder's name and starting balance."""
+        self.name = name
+        self.balance = balance
