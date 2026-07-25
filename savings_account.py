1360671ee5a87dc197dc8d58705ed48935d83fac
rosebud-lang <rhosebhurdawuah@gmail.com>
2026-07-25 19:57:41 +0100

    creating a class named Savings_account

--- savings_account.py
+++ savings_account.py
@@ -1,1 +1,9 @@
 from bank_account import BankAccount 
+
+class SavingsAccount(BankAccount):
+    """Represents a savings account that earns interest."""
+
+    def __init__(self, name, balance, interest_rate):
+        """Initializes the savings account."""
+        super().__init__(name, balance)
+        self.interest_rate = interest_rate
