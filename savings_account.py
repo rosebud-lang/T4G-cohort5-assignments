721ec1a4876a9b3d0a5e3a7d688e8923dffaf8ba
rosebud-lang <rhosebhurdawuah@gmail.com>
2026-07-25 20:04:59 +0100

    this function calculates interest and deposits it into the account

--- savings_account.py
+++ savings_account.py
@@ -7,3 +7,12 @@ class SavingsAccount(BankAccount):
         """Initializes the savings account."""
         super().__init__(name, balance)
         self.interest_rate = interest_rate
+
+
+    def apply_interest(self):
+        """Calculates interest and deposits it into the account."""
+
+        interest = self.balance * (self.interest_rate / 100)
+        self.deposit(interest)
+
+        print(f"Interest of GHS {interest:.2f} applied.")
